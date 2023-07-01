8511dbfa392ad44a9362080a4eca8f8531afbb59
Filip Žitný <filipzitny@gmail.com>
2023-07-01 17:38:47 +0200

    commint

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 
 
 if __name__ == '__main__':
-    print('src is runing xD')
+    print('Game is runing xD')
--- map/generator.py
+++ map/generator.py
@@ -1,1 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Map Generator"""
+
 import numpy as np
+
+class MapGenerator:
+    """Class that generate map"""
+    def __init__(self) -> None:
+        """Init"""
+        pass
+
+    def hallway(self):
+        """Hallway generator"""
+        pass
+
+    def chamber(self):
+        """Chamber generator"""
+        pass
+
+    def teleport(self):
+        pass
+
+    def heal(self):
+        pass
+
