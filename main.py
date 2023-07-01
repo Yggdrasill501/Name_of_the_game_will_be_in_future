366d3a7da98707cb62d7f194b03e3f6f97b20c84
Filip Žitný <filipzitny@gmail.com>
2023-07-01 17:57:24 +0200

    game event

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 """Run whole project form here"""
+import pygame
+from sys import exit
+#from random import randint, choice
 
+game_active: bool = True
 
 if __name__ == '__main__':
-    print('Game is runing xD')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            if game_active:
+                print('Game is runing xD')
