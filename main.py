c3e2c42b23d09536ac6518306f074d588ab918de
Filip Žitný <filipzitny@gmail.com>
2023-07-01 18:12:24 +0200

    player update main update

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import pygame
 from sys import exit
 
 game_active: bool = True
+game_is_running: bool = True
 
 if __name__ == '__main__':
-    while True:
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    background = pygame.image.load('')
+
+    while game_is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
             if game_active:
-                print('Game is runing xD')
+                print('Game is running xD')
